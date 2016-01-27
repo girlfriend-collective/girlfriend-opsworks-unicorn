fa794dcf87463a1f61e195292fbd25fb05ae626d
Jimmy Malone <jimmy@girlfriend.com>
2016-01-27 11:25:09 -0800

    Add CORS headers

---
+++ unicorn/metadata.rb
@@ -0,0 +1,7 @@
+name             'unicorn'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures unicorn'
+long_description 'Installs/Configures unicorn'
+version          '0.1.0'
